af437d989b3983a21352b54c4364658eda51f2ce
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-06-07 17:33:59 -0300

    feat: process midia ETL finished

--- src/newsforge/env.py
+++ src/newsforge/env.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 BUCKET = os.getenv("BUCKET")
+PROCESSED_BUCKET = os.getenv("PROCESSED_BUCKET")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
--- src/newsforge/etls/html_news.py
+++ src/newsforge/etls/html_news.py
@@ -1,12 +1,13 @@
+import boto3
 from bs4 import BeautifulSoup
 from pyspark.sql import Row
 from prefect import flow, task
 from pyspark.sql import SparkSession
-from newsforge.env import S3_ENDPOINT, BUCKET, DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD
+from newsforge.env import S3_ENDPOINT, BUCKET, DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, PROCESSED_BUCKET, \
+    AWS_SECRET_KEY, AWS_SECRET_ACCESS_KEY
 from pyspark.sql.types import StructType, StructField, StringType
-
-from newsforge.models import gemini_flash
 from newsforge.models.gemini_flash import GeminiFlash
+from botocore.client import Config
 
 
 def get_html_content(html_news: str) -> str:
@@ -22,17 +23,26 @@ def get_html_content(html_news: str) -> str:
     return result
 
 def get_structured_data(html_news: str):
-    pass
+    system_prompt: str = "You are a information extractor. Your task is to extract the required information from a news article."
+    g_flash = GeminiFlash()
+    return g_flash.handle(system_prompt, html_news)
 
 def extract_data(html_news: str):
     content_str: str = get_html_content(html_news)
-
-
-    system_prompt: str = "You are a information extractor. Your task is to extract the required information from a news article."
-    g_flash = GeminiFlash()
-    result = g_flash.handle(system_prompt, content_str)
+    result = get_structured_data(content_str)
 
     return Row(title=result["title"], origin=result["news_origin"], resume=result["resume"], transcription=result["transcription"])
+
+def list_files(s3, prefix=None, bucket=BUCKET):
+    paginator = s3.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+
+    arquivos = []
+    for page in page_iterator:
+        if "Contents" in page:
+            for obj in page["Contents"]:
+                arquivos.append(obj["Key"])
+    return arquivos
 
 @task()
 def transform_data():
@@ -53,7 +63,7 @@ def transform_data():
 
     print("Spark Session Iniciada")
 
-    rdd = spark.sparkContext.wholeTextFiles(f"s3a://{BUCKET}/*.html")
+    rdd = spark.sparkContext.wholeTextFiles(f"s3a://{BUCKET}/unprocessed/*.html")
 
     htmls = rdd.toDF(["path", "value"])
 
@@ -82,9 +92,38 @@ def transform_data():
 
     spark.stop()
 
+@task()
+def move_data_to_processed():
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_SECRET_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        endpoint_url=S3_ENDPOINT,
+        config=Config(signature_version='s3v4'),
+    )
+
+    unprocessed_files: list[str] = list_files(s3, prefix="unprocessed/")
+
+    if not unprocessed_files:
+        print("Nenhum arquivo foi processado")
+        return
+
+    for file in unprocessed_files:
+        s3.copy_object(
+            Bucket=BUCKET,
+            CopySource=f"{BUCKET}/{file}",
+            Key=file.replace("unprocessed/", "processed/"),
+        )
+        s3.delete_object(Bucket=BUCKET, Key=file)
+
 @flow(name="ETL HTML News")
 def batch():
-    transform_data()
+    try:
+        transform_data()
+        move_data_to_processed()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
